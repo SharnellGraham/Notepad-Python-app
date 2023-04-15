6f6e00c751ce5b707406ffd10d22c8461c124b08
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-04-15 13:58:53 -0400

    Added NoteModel class with title, content and date fields

--- peewee_notes_model.py
+++ peewee_notes_model.py
@@ -4,3 +4,12 @@ from peewee import *
 
 # Create the database connection to notes.db file
 db = SqliteDatabase('notes.db')
+
+# Define the NoteModel class that uses db connection as Meta class
+class NoteModel(Model):
+    title = CharField() # Charfield for storing titles
+    content = TextField() # Textfield for storing content
+    date = DateTimeField(default=datetime.datetime.now) # DateTimeField with current time as default value
+
+    class Meta:
+        database = db
