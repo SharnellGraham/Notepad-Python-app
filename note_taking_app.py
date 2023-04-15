539e0ff2e100fe363201219fb3ca7b39073f7193
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-04-15 15:18:33 -0400

    Create a new note model instance and save it

--- note_taking_app.py
+++ note_taking_app.py
@@ -8,3 +8,5 @@ seed_data()
 def create_note():
     title = input("Enter note title: ")
     content = input("Enter note content: ")
+    note = NoteModel.create(title=title, content=content)
+    print(f'Note "{note.title}" created successfully!')
